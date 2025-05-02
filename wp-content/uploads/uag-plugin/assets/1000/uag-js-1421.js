a5c39c106554ae95eb3f177c8cbaf62b780feae0
FerryJulyo <ferryjulyo86@gmail.com>
2025-05-02 11:19:12 +0700

    first commit

---
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-1421.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-8d361fa7', {"layout":"number","heading":"Home Delivery","numberPrefix":"","numberSuffix":"","startNumber":0,"endNumber":698,"totalNumber":698,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-bd3bf2cc', {"layout":"number","heading":"Happy People","numberPrefix":"","numberSuffix":"","startNumber":0,"endNumber":1106,"totalNumber":1106,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-30dc5758', {"layout":"number","heading":"Tons Of Goods","numberPrefix":"","numberSuffix":"","startNumber":0,"endNumber":4469,"totalNumber":4469,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-61326afc', {"layout":"number","heading":"Tree plant","numberPrefix":"","numberSuffix":"","startNumber":0,"endNumber":754,"totalNumber":754,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+ });
